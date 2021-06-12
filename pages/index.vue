56adad5a5d54cb862ce4790c18bfea2a7de69830
alperenkarate <alperensozen96@gmail.com>
2021-06-12 16:04:53 +0300

    static files are linked

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,7 @@ export default {
   position: relative;
   padding: 30px;
   box-sizing: border-box;
+  background-image: url("~/assets/images/bg.jpg");
   background-position: center;
   background-size: cover;
 }
