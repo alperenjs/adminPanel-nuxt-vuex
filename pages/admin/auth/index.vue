5fe1ea1a27f277613d4bc6b757a76d093c576289
alperenkarate <alperensozen96@gmail.com>
2021-06-13 00:21:47 +0300

    login & register is working

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -6,8 +6,11 @@
       <br />
       <label for="password">password</label>
       <input v-model="password" id="password" type="password" />
-      <button>Bas</button>
+      <button type="submit">{{ isLogin ? "Login" : "Kayıt Ol" }}</button>
     </form>
+    <button @click="isLogin = !isLogin">
+      Switch to {{ isLogin ? "Kayıt Ol" : "Giriş Yap" }}
+    </button>
   </div>
 </template>
 
@@ -17,21 +20,25 @@ export default {
   layout: "admin",
   data() {
     return {
+      isLogin: true,
       email: "",
       password: "",
     };
   },
   methods: {
     onSubmit() {
+      let authUrl =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBFGp0MSMWVYcxGP8bwR1qP5y0IZDLgjes";
+      if (!this.isLogin) {
+        authUrl =
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBFGp0MSMWVYcxGP8bwR1qP5y0IZDLgjes";
+      }
       axios
-        .post(
-          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBFGp0MSMWVYcxGP8bwR1qP5y0IZDLgjes",
-          {
-            email: this.email,
-            password: this.password,
-            returnSecureToken: true,
-          }
-        )
+        .post(authUrl, {
+          email: this.email,
+          password: this.password,
+          returnSecureToken: true,
+        })
         .then((result) => {
           console.log(result);
         })
