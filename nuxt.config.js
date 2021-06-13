39f4a172d867a1a279384b8b6133cbfe941b2a13
alperenkarate <alperensozen96@gmail.com>
2021-06-13 23:10:09 +0300

    firebaes run comment in plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/runFirebase.js'
+    '~plugins/runFirebase.js' //firebase i buradan başlatıyorum
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
